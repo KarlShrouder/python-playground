9e86849d7106d808a338ba2bcd4b35cbba632d01
Karl Shrouder <karl.shrouder@gmail.com>
2020-11-27 14:36:18 -0500

    Classes

--- Playground/app.py
+++ Playground/app.py
@@ -1,10 +1,50 @@
-
-try:
-    age= int(input("Age: "))
-    income = 20000
-    risk = income / age
-    print(age)
-except ValueError:
-    print("Invalid Value")
-except ZeroDivisionError:
-    print("Age cannot be zero")
+# try:
+#     age= int(input("Age: "))
+#     income = 20000
+#     risk = income / age
+#     print(age)
+# except ValueError:
+#     print("Invalid Value")
+# except ZeroDivisionError:
+#     print("Age cannot be zero")
+#
+# class Point:
+#     def move(self):
+#         print("move")
+#
+#     def draw(self):
+#         print("draw")
+#
+#
+# point1 = Point()
+# point1.x = 20
+# point1.y = 30
+# print(f"({point1.x}, {point1.y})")
+# point1.draw()
+# point1.move()
+#
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#
+#     def move(self):
+#         print("move")
+#
+#     def draw(self):
+#         print("draw")
+#
+# point = Point(122, 12)
+# print(point.x)
+# print(point.y)
+#
+# class Person:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def talk(self):
+#         print(f"{self.name}: Blah")
+#
+#
+# person = Person("Karl")
+# person.talk()
