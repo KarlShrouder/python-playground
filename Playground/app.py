9ab07fd1123a3b09465d0dd7bb51d9e8f71d2c26
Karl <Karl.shrouder@gmail.com>
2020-11-30 23:12:40 -0500

    Practice
    Packages, Rand Values, Files and Directories, PyPi

--- Playground/app.py
+++ Playground/app.py
@@ -80,7 +80,7 @@
 # kitty.walk()
 # kitty.meow()
 
-# ------------(Modules)--------------------
+# ------------(Modules)--------------
 # import converters
 # from converters import kg_to_lbs
 #
@@ -88,6 +88,59 @@
 #
 # print(converters.kg_to_lbs(70))
 
-# -------------(Modules Exercise)-------------------
+# -------------(Modules Exercise)--------
+# from utils import find_max
+#
+# numbers = [12, 14, 11, 3, 7, 54, 65]
+# maximum = find_max(numbers)
+# print(maximum)
 
+# ------------(Packages)------------------------
+# from ecommerce.shipping import cal_shipping
+# cal_shipping()
+
+# --------(Generating Rand Values)-----------------------
+# import random
+#
+# for i in range(3):
+#     print(random.randint(10, 20))
+#
+# members = ["Karl", "Dom", "Maggie"]
+# leader = random.choice(members)
+# print(leader)
+
+# --------(Generating Rand Values Ex.)-----------------------
+# import random
+#
+#
+# class Dice:
+#     def __init__(self):
+#         pass
+#
+#     @staticmethod
+#     def roll():
+#         first = random.randint(1, 6)
+#         second = random.randint(1, 6)
+#         return first, second
+#
+#
+# dice = Dice()
+# print(dice.roll())
+
+# ----------------(Files and Directories)-------------------------
+# from pathlib import Path
+#
+# # Absolute Path
+# # c:\Program Files\Microsoft
+# # /usr/local/bin
+#
+# # Relative Path
+# # from the working directory
+#
+# path = Path("ecommerce")
+# print(path.exists())
+#
+# path = Path()
+# for file in path.glob("*"):
+#     print(file)
 
