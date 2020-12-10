c850385ee489f10f5eb92ea94e0486c3a00724b0
Karl <Karl.shrouder@gmail.com>
2020-12-10 16:29:27 -0500

    Pygame
    Pygame Tutorial. Basic movement of sprites around a game window

--- Playground/app.py
+++ Playground/app.py
@@ -189,5 +189,57 @@
 #   * MatPlotLib
 #   * Scikit-Learn
 
+# ----------------------(Pygame Tutorial)----------------------------
+import pygame
 
+# Initialize pygame
+pygame.init()
 
+# Create the screen
+screen = pygame.display.set_mode((800, 600))
+
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+playerY_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+# Game loop
+running = True
+while running:
+
+    # Screen RGB
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+            if event.key == pygame.K_UP:
+                playerY_change = -0.1
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
+
+    playerX += playerX_change
+    playerY += playerY_change
+    player(playerX, playerY)
+    pygame.display.update()
